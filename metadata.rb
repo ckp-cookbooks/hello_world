0bc71ced81991be1b7dff4ac775fba97e4ca5148
caryp <cary@rightscale.com>
2013-08-12 16:59:21 -0700

    removed used of VERSION file due to test-kitchen limitation

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "cary@rightscale.com"
 license          "Apache 2.0"
 description      "Logs hello_world to the chef log"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
+version          "0.0.3"
 
