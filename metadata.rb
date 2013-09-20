149710f471505c828b5158b99f866b0c37deb2da
caryp <cary@rightscale.com>
2013-09-20 13:24:02 -0700

    added default recipe to metadata

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,4 @@ description      "Logs hello_world to the chef log"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.3"
 
+recipe "hello_world::default", "logs hello_world to Chef log."
